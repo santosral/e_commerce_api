4c4b87e6fd2362152198e5bc08c9ad3ab2146999
santosral <sralphian029@gmail.com>
2024-10-21 05:08:33 +0800

    Add some global exception handling for the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :not_found
+  rescue_from ActionController::ParameterMissing, with: :handle_missing_parameter
+
+  private
+
+  def not_found
+    render json: { error: "Resource not found" }, status: :not_found
+  end
+
+  def handle_missing_parameter(exception)
+    Rails.logger.error("Missing parameter: #{exception.message}")
+    render json: { error: exception.message }, status: :bad_request
+  end
 end
